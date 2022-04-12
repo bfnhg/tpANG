07f19ca1b17197fe93f9d1e1f960b3ed889cd493
bfnhg <adhamaaa890@gmail.com>
2022-04-12 16:19:04 +0000

    adhamCOM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CommandesComponent } from './commandes/commandes.component';
+import { CommandeCreateComponent } from './commandes/commande-create/commande-create.component';
+import { CommandeListComponent } from './commandes/commande-list/commande-list.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommandesComponent,
+    CommandeCreateComponent,
+    CommandeListComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
